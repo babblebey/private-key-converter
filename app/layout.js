ec658bb0a5c021c995a44f2043037008b46fded6
babblebey <babblebey@gmail.com>
2024-03-04 21:35:29 +0100

    feat: integrate dropbox in layout

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Head from "@/components/Head";
 import { Inter } from "next/font/google";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { Dropbox } from "@/components/Dropbox";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className={inter.className}>
       <Head />
       <body className="relative min-h-screen bg-black bg-gradient-to-tr from-zinc-900/50 to-zinc-700/30">
+        <Dropbox />
         <Header />
         <main className="min-h-[70vh] pb-5">
           { children }
